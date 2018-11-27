dfc92f5440c4929fdbeffed34a5e3c1676854087
unknown <vipwangxiaofeng@163.com>
2018-11-27 19:18:04 +0800

    初次构架后台框架

---
+++ src/views/layout/components/Sidebar/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <el-scrollbar wrap-class="scrollbar-wrapper">
+    <el-menu
+      :show-timeout="200"
+      :default-active="$route.path"
+      :collapse="isCollapse"
+      mode="vertical"
+      background-color="#EFF2F5"
+    >
+      <!-- background-color="#1991fc"
+      text-color="#fff"
+      active-text-color="#D4D9DD" -->
+      <sidebar-item v-for="route in routes" :key="route.path" :item="route" :base-path="route.path"/>
+    </el-menu>
+  </el-scrollbar>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+import SidebarItem from './SidebarItem'
+
+export default {
+  components: { SidebarItem },
+  computed: {
+    ...mapGetters([
+      'sidebar'
+    ]),
+    routes() {
+      console.log('路由', this.$router)
+      return this.$router.options.routes
+    },
+    isCollapse() {
+      return !this.sidebar.opened
+    }
+  }
+}
+</script>
