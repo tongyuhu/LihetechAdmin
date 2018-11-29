c99d620488672bbcfbc31a7913802dab17d0a674
unknown <vipwangxiaofeng@163.com>
2018-11-29 18:45:51 +0800

    feat:mock数据

---
+++ src/mock/index.js
@@ -0,0 +1,42 @@
+import Mock from 'mockjs'
+import loginAPI from './login'
+import articleAPI from './article'
+import remoteSearchAPI from './remoteSearch'
+import transactionAPI from './transaction'
+
+// 修复在使用 MockJS 情况下，设置 withCredentials = true，且未被拦截的跨域请求丢失 Cookies 的问题
+// https://github.com/nuysoft/Mock/issues/300
+Mock.XHR.prototype.proxy_send = Mock.XHR.prototype.send
+Mock.XHR.prototype.send = function() {
+  if (this.custom.xhr) {
+    this.custom.xhr.withCredentials = this.withCredentials || false
+  }
+  this.proxy_send(...arguments)
+}
+
+// Mock.setup({
+//   timeout: '350-600'
+// })
+
+// 登录相关
+// Mock.mock('/login', 'post', loginAPI.loginByUsername)
+// Mock.mock('/logout', 'get', loginAPI.logout)
+// Mock.mock('/info', 'get', loginAPI.getUserInfo)
+Mock.mock(/\/login/, 'post', loginAPI.loginByUsername)
+Mock.mock(/\/logout/, 'get', loginAPI.logout)
+Mock.mock(/\/info/, 'post', loginAPI.getUserInfo)
+
+// 文章相关
+Mock.mock(/\/article\/list/, 'get', articleAPI.getList)
+Mock.mock(/\/article\/detail/, 'get', articleAPI.getArticle)
+Mock.mock(/\/article\/pv/, 'get', articleAPI.getPv)
+Mock.mock(/\/article\/create/, 'post', articleAPI.createArticle)
+Mock.mock(/\/article\/update/, 'post', articleAPI.updateArticle)
+
+// 搜索相关
+Mock.mock(/\/search\/user/, 'get', remoteSearchAPI.searchUser)
+
+// 账单相关
+Mock.mock(/\/transaction\/list/, 'get', transactionAPI.getList)
+
+export default Mock
--- src/utils/request.js
+++ src/utils/request.js
@@ -13,7 +13,7 @@ const service = axios.create({
 service.interceptors.request.use(
   config => {
     if (store.getters.token) {
-      config.headers['X-Token'] = getToken() // 让每个请求携带自定义token 请根据实际情况自行修改
+      // config.headers['X-Token'] = getToken() // 让每个请求携带自定义token 请根据实际情况自行修改
     }
     return config
   },
@@ -30,7 +30,7 @@ service.interceptors.response.use(
     /**
      * code为非20000是抛错 可结合自己业务进行修改
      */
-    const res = response.data
+    // const res = response.data
     // if (res.code !== 20000) {
     //   Message({
     //     message: res.message,
@@ -58,7 +58,7 @@ service.interceptors.response.use(
     // } else {
     //   return response.data
     // }
-    return response.data
+    return response
   },
   error => {
     console.log('err' + error) // for debug
