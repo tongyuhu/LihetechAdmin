fce2ad41d5b85e84bc426fb31d93330d7e04e02f
unknown <vipwangxiaofeng@163.com>
2019-01-14 18:36:46 +0800

    fiexd:接口错误提示

--- src/utils/request.js
+++ src/utils/request.js
@@ -47,31 +47,37 @@ service.interceptors.response.use(
    * 以下代码均为样例，请结合自生需求加以修改，若不需要，则可删除
    */
   response => {
-  //   const res = response.data
-  //   if (res.code !== 20000) {
-  //     Message({
-  //       message: res.message,
-  //       type: 'error',
-  //       duration: 5 * 1000
-  //     })
-  //     // 50008:非法的token; 50012:其他客户端登录了;  50014:Token 过期了;
-  //     if (res.code === 50008 || res.code === 50012 || res.code === 50014) {
-  //       // 请自行在引入 MessageBox
-  //       // import { Message, MessageBox } from 'element-ui'
-  //       MessageBox.confirm('你已被登出，可以取消继续留在该页面，或者重新登录', '确定登出', {
-  //         confirmButtonText: '重新登录',
-  //         cancelButtonText: '取消',
-  //         type: 'warning'
-  //       }).then(() => {
-  //         store.dispatch('FedLogOut').then(() => {
-  //           location.reload() // 为了重新实例化vue-router对象 避免bug
-  //         })
-  //       })
-  //     }
-  //     return Promise.reject('error')
-  //   } else {
-      return response.data
-  //   }
+    // Message({
+    //   message: res.message,
+    //   type: 'error',
+    //   duration: 5 * 1000
+    // })
+    const res = response.data
+    if (res.code === '1005') {
+      window.location.href = '/operat/login'
+      return Promise.reject('error')
+    } else if(res.code === '1101') {
+      Message({
+        message: res.msg,
+        type: 'error',
+        duration: 5 * 1000
+      })
+      window.location.href = '/operat/login'
+    } else if(res.code === '1102') {
+      Message({
+        message: '数据更新，请重新登录',
+        type: 'error',
+        duration: 5 * 1000
+      })
+    } else if(res.code !== '0000') {
+      Message({
+        message: res.msg,
+        type: 'error',
+        duration: 5 * 1000
+      })
+    }  else {
+    return response.data
+  }
   },
   error => {
     if (error && error.response) {
@@ -81,10 +87,6 @@ service.interceptors.response.use(
           break
 
         case 401:
-          // sessionStorage.clear()
-          // window.location.href = '/BPWatch/admin/login/page'
-          // router.replace({path: '/login'})
-          // location.reload()
           error.message = '未授权，请登录'
           break
 
